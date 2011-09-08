fc374679b01aba6805b0c5a95c9460021c8242b8
James Sanders <jsanders@comverge.com>
2011-09-08 10:40:18 -0600

    Working version of primes kata

--- public/katas/prime-factors/lib/prime-factors.js
+++ public/katas/prime-factors/lib/prime-factors.js
@@ -1,5 +1,17 @@
 (function() {
-  window.primeFactors = function() {
-    return [];
+  window.primeFactors = function(num) {
+    var primes = [];
+    var candidate = 2;
+    while(num > 1) {
+      while(num % candidate == 0) {
+        primes.push(candidate);
+        num = num / candidate;
+      }
+      candidate = candidate + 1;
+    }
+    if(num > 1) {
+      primes.push(num);
+    }
+    return primes;
   };
 })();
--- public/katas/prime-factors/spec/prime-factors-spec.js
+++ public/katas/prime-factors/spec/prime-factors-spec.js
@@ -5,4 +5,28 @@ describe("#primeFactors", function() {
   it("finds 2 as a prime factor of 2", function() {
     expect(primeFactors(2)).toEqual([2]);
   });
+  it("finds 3 as a prime factor of 3", function() {
+    expect(primeFactors(3)).toEqual([3]);
+  });
+  it("finds [2, 2] as prime factors of 4", function() {
+    expect(primeFactors(4)).toEqual([2, 2]);
+  });
+  it("finds [2, 3] as prime factors of 6", function() {
+    expect(primeFactors(6)).toEqual([2, 3]);
+  });
+  it("finds [2, 2, 2] as prime factors of 8", function() {
+    expect(primeFactors(8)).toEqual([2, 2, 2]);
+  });
+  it("finds [3, 3] as prime factors of 9", function() {
+    expect(primeFactors(9)).toEqual([3, 3]);
+  });
+  it("finds [5, 2] as prime factors of 10", function() {
+    expect(primeFactors(10)).toEqual([2, 5]);
+  });
+  it("finds [11] as prime factors of 11", function() {
+    expect(primeFactors(11)).toEqual([11]);
+  });
+  it("finds [2, 2, 3] as prime factors of 12", function() {
+    expect(primeFactors(12)).toEqual([2, 2, 3]);
+  });
 });
